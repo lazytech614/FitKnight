0088553e1b8ff0e1e96f9dae190858a195bc6715
lazytech614 <derupanjan2021@gmail.com>
2025-01-26 04:32:34 +0530

    update UI

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,7 +7,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 export function Input({ className = '', ...props }: InputProps) {
   return (
     <input
-      className={`w-full p-2 border border-gray-300 rounded-lg outline-none ${className}`}
+      className={`w-fit p-2 border border-gray-300 rounded-lg outline-none ${className}`}
       {...props}
     />
   );
